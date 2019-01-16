6fcf6ffde2c45733ad4fb1cc0e57256180f2b617
wangkaiwd <1162486176@qq.com>
2019-01-16 21:16:06 +0800

    feat: 搭建服务器，并初步理解源码

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const Koa = require('koa')
+// 新建一个Application实例
+const app = new Koa()
+const port = 2334
+// 调用Application原型上的方法，传入一个回调函数，放入中间位件数组，将实例对象返回
+// 如果是generator函数会走另外的逻辑 
+app.use(async (ctx, next) => {
+  ctx.body = "request success"
+})
+// app.listen : 通过http模块创建服务并监听
+app.listen(port, err => {
+  if (!err) { console.log(`服务启动成功，监听${port}端口!`) }
+})
