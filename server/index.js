21b885755d50f31abcfb63c52caf854ba795351b
wangkaiwd <1162486176@qq.com>
2019-01-16 23:27:31 +0800

    feat: 静态文件加载 通过koa-views实现了静态静态文件的加载(.pug)

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
 const Koa = require('koa')
 const pug = require('pug')
+const { resolve } = require('path')
+const views = require('koa-views')
 // 新建一个Application实例
 const app = new Koa()
-const { normalTpl, pugTpl } = require('./template')
 const port = 2334
+
+// 加载模板引擎，指定路径为当前的views目录，扩展名是pug
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 // 调用Application原型上的方法，传入一个回调函数，放入中间位件数组，将实例对象返回
 // 如果是generator函数会走另外的逻辑 
 app.use(async (ctx, next) => {
   ctx.type = 'text/html;charset=utf-8'
-  ctx.body = pug.render(pugTpl, {
-    you: 'koa',
-    me: 'wk'
+  // ./当前路径是相对于当前进程
+  // ctx.body = pug.renderFile(resolve(__dirname, './template/test.pug'), {
+  //   you: 'koa',
+  //   me: 'wk'
+  // })
+  // 使用了koa-views中间件后，将pug.render方法绑定到了ctx上
+  await ctx.render('demo', {
+    name: 'AAA'
   })
 })
 // app.listen : 通过http模块创建服务并监听
